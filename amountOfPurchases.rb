5a2d373d74a129df86fe821b2f1384eaf43835b8
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-03-17 01:17:06 +0300

    introductory task

--- amountOfPurchases.rb
+++ amountOfPurchases.rb
@@ -6,16 +6,16 @@ loop do
     name = gets.chomp
     break if name == 'стоп'
     print 'Кол-во товаров: '
-    amount = gets.chomp
+    amount = gets.chomp.to_i
     print 'Цена за ед: '
-    price = gets.chomp
+    price = gets.chomp.to_f
 
-    cart[name] = {'itemPrice': price, 'amount': amount, 'price': price.to_i * amount.to_i}
+    cart[name] = {'itemPrice': price, 'amount': amount, 'price': price * amount}
 end
 
 cart.each do |name, props|
     puts "#{name} - Кол-во: #{props[:amount]}, Цена за ед: #{props[:itemPrice]}, Общая цена: #{props[:price]}"
-    summ += props[:price].to_i
+    summ += props[:price]
 end
 
 puts "Стоимость: #{summ}"
