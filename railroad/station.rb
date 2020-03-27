c6aa944a1114c8ca229d1e484db05aa7638d80f8
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-03-27 10:05:52 +0300

    lesson3

--- railroad/station.rb
+++ railroad/station.rb
@@ -1,9 +1,20 @@
+require_relative './module_instance_counter.rb'
+
 class Station
+  include InstanceCounter
   attr_reader :name, :trains
+
+  @@stations = []
+
+  def self.all
+    puts @@stations
+  end
 
   def initialize(name)
     @name = name
     @trains = []
+    @@stations << self
+    register_instance
   end
 
   def add_train(train)
